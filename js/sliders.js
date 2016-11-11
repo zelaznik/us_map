c0e8545baf5729585526b59ae9d972f895ed7e10
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2016-11-11 07:58:49 -0500

    Got the overflow electoral vote statusbar to work.

--- js/sliders.js
+++ js/sliders.js
@@ -1,17 +1,19 @@
 $( function() {
-  var $johnson_slider                    = $('#blue'),
-      $stein_slider                      = $('#green'),
-      $green_and_blue                    = $('#green, #blue'),
-      $stein_percent_for_clinton         = $('#stein_percent_for_clinton'),
-      $johnson_percent_for_clinton       = $('#johnson_percent_for_clinton'),
-      $stein_percent_for_trump           = $('#stein_percent_for_trump'),
-      $johnson_percent_for_trump         = $('#johnson_percent_for_trump'),
-      $electoral_progress_bar            = $('#electoral_progress_bar'),
-      $electoral_debugger                = $('#electoral_debugger'),
-      $electoral_progress_bar_clinton    = $('#electoral_progress_bar_clinton'),
-      $electoral_progress_bar_trump      = $('#electoral_progress_bar_trump'),
-      $electoral_text_bar_clinton        = $('#electoral_progress_bar_clinton > p'),
-      $electoral_text_bar_trump          = $('#electoral_progress_bar_trump   > p');
+  var $johnson_slider                          = $('#blue'),
+      $stein_slider                            = $('#green'),
+      $green_and_blue                          = $('#green, #blue'),
+      $stein_percent_for_clinton               = $('#stein_percent_for_clinton'),
+      $johnson_percent_for_clinton             = $('#johnson_percent_for_clinton'),
+      $stein_percent_for_trump                 = $('#stein_percent_for_trump'),
+      $johnson_percent_for_trump               = $('#johnson_percent_for_trump'),
+      $electoral_progress_bar                  = $('#electoral_progress_bar'),
+      $electoral_debugger                      = $('#electoral_debugger'),
+      $electoral_progress_bar_clinton          = $('#electoral_progress_bar_clinton'),
+      $electoral_progress_bar_trump            = $('#electoral_progress_bar_trump'),
+      $electoral_text_bar_clinton              = $('#electoral_progress_bar_clinton > p'),
+      $electoral_text_bar_trump                = $('#electoral_progress_bar_trump   > p'),
+      $electoral_progress_bar_clinton_extra    = $('#electoral_progress_bar_clinton_extra'),
+      $electoral_progress_bar_trump_extra      = $('#electoral_progress_bar_trump_extra');
 
   function electors(data) {
     var tally = {clinton: 0, trump: 0};
@@ -38,11 +40,16 @@ $( function() {
       old_clinton_count = electoral_total.clinton;
     }
 
-    var total = (electoral_total.clinton + electoral_total.trump),
-        alpha =  Math.round(100* electoral_total.clinton / total);
+    var total         =  (electoral_total.clinton + electoral_total.trump),
+        clinton_width =  Math.round(100* electoral_total.clinton / total),
+        trump_width   =  100 - clinton_width,
+        max_width     =  50;
 
-    $electoral_progress_bar_clinton.attr('style', 'width: ' + alpha       + '%; transition: none; ');
-    $electoral_progress_bar_trump.attr('style',   'width: ' + (100-alpha) + '%; transition: none; ');
+    $electoral_progress_bar_clinton.attr(       'style',   'width: ' + Math.min(max_width, clinton_width)       + '%; transition: 100ms; ');
+    $electoral_progress_bar_clinton_extra.attr( 'style',   'width: ' + Math.max(0,  clinton_width - max_width)  + '%; transition: 100ms; ');
+
+    $electoral_progress_bar_trump_extra.attr(   'style',   'width: ' + Math.max(0, trump_width - max_width)     + '%; transition: 100ms; ');
+    $electoral_progress_bar_trump.attr(         'style',   'width: ' + Math.min(max_width, trump_width)         + '%; transition: 100ms; ');
 
     $electoral_text_bar_clinton.text('Clinton (' + electoral_total.clinton + ')');
     $electoral_text_bar_trump.text('(' + electoral_total.trump + ') Trump');
